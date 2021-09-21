c01576392cff5d4384536c2cbf5f15e6007beb4c
Aashutosh Mallik <Aashu.Mallik@gofastpath.com>
2021-09-21 11:32:25 -0500

    Add project files.

---
+++ AspNetWebApiRest/Controllers/ListItemsController.cs
@@ -0,0 +1,54 @@
+﻿using AspNetWebApiRest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AspNetWebApiRest.Controllers
+{
+    public class ListItemsController : ApiController
+    {
+        private static List<CustomListItem> _listItems { get; set; } = new List<CustomListItem>();
+        // GET api/<controller>
+        public IEnumerable<CustomListItem> Get()
+        {
+            return _listItems;
+        }
+
+        // GET api/<controller>/5
+        public HttpResponseMessage Get(int id)
+        {
+            var item = _listItems.FirstOrDefault(x => x.Id == id);
+            if (item != null)
+            {
+                return Request.CreateResponse(HttpStatusCode.OK, item);
+            }
+            return Request.CreateResponse(HttpStatusCode.NotFound);
+        }
+        // POST api/<controller>
+        public void Post([FromBody] CustomListItem model)
+        {
+            _listItems.Add(model);
+           // return Request.CreateResponse(HttpStatusCode.Created, model);
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public HttpResponseMessage Delete(int id)
+        {
+            var item = _listItems.FirstOrDefault(x => x.Id == id);
+            if (item != null)
+            {
+                _listItems.Remove(item);
+                return Request.CreateResponse(HttpStatusCode.OK, item);
+            }
+            return Request.CreateResponse(HttpStatusCode.NotFound);
+        }
+    }
+}
